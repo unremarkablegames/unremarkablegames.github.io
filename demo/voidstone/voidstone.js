ded923abd0b6ae91fb5d4b5daf32ba6d9c390cfc
Karl McClendon <kmcclendo@utilimarc.com>
2025-12-08 08:26:20 -0600

    sdfsdf

--- demo/voidstone/voidstone.js
+++ demo/voidstone/voidstone.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmpk70ri18t.js
+// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp83757z2t.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -206,21 +206,21 @@ Module['FS_createPath']("/rsrc", "fonts", true, true);
 
   })();
 
-// end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmpk70ri18t.js
-// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp692x0r31.js
+// end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp83757z2t.js
+// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmpsk6_mqsb.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp692x0r31.js
-// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp6mwhq_tb.js
+  // end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmpsk6_mqsb.js
+// include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp1ohvmm45.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp6mwhq_tb.js
+  // end include: /var/folders/jy/bxbpslls2934wxp0hn4m6qwr0000gp/T/tmp1ohvmm45.js
 
 
 var arguments_ = [];
